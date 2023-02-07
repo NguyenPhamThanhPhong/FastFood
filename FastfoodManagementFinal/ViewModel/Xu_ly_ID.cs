74123ceee897ea169a4adc5b6726ede3be284c34
Phong <thanhphong17160@gmail.com>
2023-02-07 19:51:54 +0700

    update xu ly ID

--- FastfoodManagementFinal/ViewModel/Xu_ly_ID.cs
+++ FastfoodManagementFinal/ViewModel/Xu_ly_ID.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.Configuration;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using Microsoft.Office.Interop.Excel;
 
 namespace FastfoodManagementFinal.ViewModel
 {
@@ -18,155 +19,133 @@ namespace FastfoodManagementFinal.ViewModel
         public static string GetStaffID(string accessright)
         {
             List<Account> acc = new List<Account>();
-            int count = 1;
             if (con.State != ConnectionState.Open)
             {
                 con.Open();
-                string sql = "select ID from staff order by ID asc";
+                string sql = "select * from staff where acessRight = N'"+accessright+"' order by ID asc";
                 SqlCommand cmd = new SqlCommand(sql, con);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    string MS = reader.GetString(0);
-                    bool isHaveMS = false;
-                    switch (accessright)
-                    {
-                        case "Nhân viên":
-                            {
-                                if (MS.Substring(0, 2).Equals("NV"))
-                                {
-                                    string numberId = reader.GetString(0).Substring(2);
-                                    if (count == int.Parse(numberId))
-                                    {
-                                        count++;
-                                    }
-                                    else
-                                    {
-                                        isHaveMS = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            break;
-                        case "Quản lý":
-                            {
-                                if (MS.Substring(0, 2).Equals("QL"))
-                                {
-                                    string numberId = reader.GetString(0).Substring(2);
-                                    if (count == int.Parse(numberId))
-                                    {
-                                        count++;
-                                    }
-                                    else
-                                    {
-                                        isHaveMS = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            break;
-                    }
+                    Account a = new Account();
+                    a.StaffID = reader.GetString(0);
+                    a.Avatar = reader.GetString(1);
+                    a.AccessRight = reader.GetString(2);
+                    a.Username = reader.GetString(3);
+                    a.Pass = reader.GetString(4);
+                    a.Name = reader.GetString(5);
+                    a.Sex = reader.GetString(6);
+                    a.DateOfBirth = reader.GetDateTime(7);
+                    a.Phone_Number = reader.GetString(8);
+                    a.Email = reader.GetString(9);
+                    a.address = reader.GetString(10);
+                    acc.Add(a);
+                }
+                con.Close();
+            }
+            string prefix = "NV";
+            if(accessright=="Quản lý")
+            {
+                prefix = "QL";
+            }
 
-                    if (isHaveMS)
-                    {
-                        con.Close();
-                        break;
-                    }
+            for (int i=0; i< acc.Count - 1;i++)
+            {
+                int pre = int.Parse(acc[i].StaffID.Substring(2, 3));
+                int next = int.Parse(acc[i + 1].StaffID.Substring(2, 3));
+                if (next - pre > 1)
+                {
+                    return prefix + (pre + 1).ToString().PadLeft(3, '0');
                 }
             }
-            switch (accessright)
-            {
-                case "Nhân viên":
-                    return "NV" + count.ToString().PadLeft(3, '0');
-                case "Quản lý":
-                    return "QL" + count.ToString().PadLeft(3, '0');
-                default:
-                    return "";
-            }
+            return prefix + (acc.Count + 1).ToString().PadLeft(3, '0');
+            
         }
         public static string GetProductID()
         {
-            List<Account> acc = new List<Account>();
-            int count = 1;
+            List<Product> p = new List<Product>();
             if (con.State != ConnectionState.Open)
             {
                 con.Open();
-                string sql = "select ProductID from PRODUCTS order by ProductID asc";
+                string sql = "select * from Products order by productID asc ";
                 SqlCommand cmd = new SqlCommand(sql, con);
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    string MS = reader.GetString(0);
-                    bool isHaveMS = false;
-                    if (MS.Substring(0, 2).Equals("SP"))
-                    {
-                          string numberId = reader.GetString(0).Substring(2);
-                          if (count == int.Parse(numberId))
-                          {
-                              count++;
-                          }
-                          else
-                          {
-                              isHaveMS = true;
-                              break;
-                          }
-                    }
-                    if (isHaveMS)
-                    {
-                        con.Close();
-                    }
+                    Product product = new Product();
+                    product.ProductId = reader.GetString(0);
+                    product.Name = reader.GetString(1);
+                    product.Product_Type = reader.GetString(2);
+                    product.Price = reader.GetInt32(3);
+                    product.Remaining_quantity = reader.GetInt32(4);
+                    product.description = reader.GetString(5);
+                    product.Avatar = reader.GetString(6);
+                    p.Add(product);
+                }
+                con.Close();
+            }
+            
+            for (int i = 0; i < p.Count - 1; i++)
+            {
+                int pre = int.Parse(p[i].ProductId.Substring(2, 3));
+                int next = int.Parse(p[i + 1].ProductId.Substring(2, 3));
+                if (next - pre > 1)
+                {
+                    return "SP" + (pre + 1).ToString().PadLeft(3, '0');
                 }
             }
-            return "SP" + count.ToString().PadLeft(3, '0');
+            return "SP" + (p.Count + 1).ToString().PadLeft(3, '0');
         }
         public static string GetCustomerID()
         {
-            List<Customer> csm = Xu_Ly_SQL.Select_all_Customers();
-            for(int i=0;i<csm.Count-1;i++)
+            List<Customer> csm = new List<Customer>();
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+                string sql = "select * from customers order by customerID asc ";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Customer c = new Customer();
+                    c.CustomerID = reader.GetString(0);
+                    c.CustomerName = reader.GetString(1);
+                    c.CustomerSex = reader.GetString(2);
+                    c.CustomerPhone = reader.GetString(3);
+                    c.CustomerTotal = reader.GetInt32(4);
+                    c.CustomerRank = reader.GetString(5);
+                    c.Address = reader.GetString(6);
+                    csm.Add(c);
+                }
+
+                con.Close();
+            }
+            for (int i=0;i<csm.Count-1;i++)
             {
                 int pre = int.Parse(csm[i].CustomerID.Substring(2, 3));
                 int next = int.Parse(csm[i + 1].CustomerID.Substring(2, 3));
                 if(next-pre>1)
                 {
+
                     return "KH" + (pre+1).ToString().PadLeft(3, '0');
                 }
             }
             return "KH" + (csm.Count+1).ToString().PadLeft(3, '0');
         }
+
         public static string GetBillID()
         {
-            List<Account> acc = new List<Account>();
-            int count = 1;
-            if (con.State != ConnectionState.Open)
+            List<Bill> bills = Xu_Ly_SQL.Select_all_Bill();
+            for (int i = 0; i < bills.Count-1; i++)
             {
-                con.Open();
-                string sql = "select BillID from BILL order by BillID asc";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                int pre = int.Parse(bills[i].Bill_ID.Substring(2, 3));
+                int next = int.Parse(bills[i + 1].Bill_ID.Substring(2, 3));
+                if (next - pre > 1)
                 {
-                    string MS = reader.GetString(0);
-                    bool isHaveMS = false;
-                    if (MS.Substring(0, 2).Equals("HD"))
-                    {
-                        string numberId = reader.GetString(0).Substring(2);
-                        if (count == int.Parse(numberId))
-                        {
-                            count++;
-                        }
-                        else
-                        {
-                            isHaveMS = true;
-                            break;
-                        }
-                    }
-                    if (isHaveMS)
-                    {
-                        con.Close();
-                    }
+                    return "HD" + (pre + 1).ToString().PadLeft(3, '0');
                 }
             }
-            return "HD" + count.ToString().PadLeft(3, '0');
+            return "HD" + (bills.Count+1).ToString().PadLeft(3, '0');
         }
         public static string GetOrderID(int i, string billID)
         {
@@ -174,38 +153,17 @@ namespace FastfoodManagementFinal.ViewModel
         }
         public static string GetImportID()
         {
-            List<Account> acc = new List<Account>();
-            int count = 1;
-            if (con.State != ConnectionState.Open)
+            List<Import> imps = Xu_Ly_SQL.Select_all_Import();
+            for (int i = 0; i < imps.Count-1; i++)
             {
-                con.Open();
-                string sql = "select ImportID from IMPORT order by ImportID asc";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                int pre = int.Parse(imps[i].ImportID.Substring(2, 3));
+                int next = int.Parse(imps[i + 1].ImportID.Substring(2, 3));
+                if (next - pre > 1)
                 {
-                    string MS = reader.GetString(0);
-                    bool isHaveMS = false;
-                    if (MS.Substring(0, 2).Equals("NH"))
-                    {
-                        string numberId = reader.GetString(0).Substring(2);
-                        if (count == int.Parse(numberId))
-                        {
-                            count++;
-                        }
-                        else
-                        {
-                            isHaveMS = true;
-                            break;
-                        }
-                    }
-                    if (isHaveMS)
-                    {
-                        con.Close();
-                    }
+                    return "NH" + (pre + 1).ToString().PadLeft(3, '0');
                 }
             }
-            return "NH" + count.ToString().PadLeft(3, '0');
+            return "NH" + (imps.Count+1).ToString().PadLeft(3, '0');
         }
         public static string GetImportProductID(int i, string ImportID)
         {
